c509f469df4c88ab5300751eaca1dd160cf3bf25
kevmac415 <kevmcfarland@yahoo.com>
2021-01-08 21:21:19 -0800

    Redux state Managment

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -10,6 +10,15 @@ import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
+
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPromotions,
+    fetchPartners
+}
 
 
 const DirectoryNavigator = createStackNavigator(
@@ -197,7 +206,18 @@ const MainNavigator = createDrawerNavigator(
 );
 
 const AppNavigator = createAppContainer(MainNavigator);
+
+    
+
 class Main extends Component {
+    
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPromotions();
+        this.props.fetchPartners();
+    }
+    
     render() {
         return (
             <View
@@ -241,4 +261,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
