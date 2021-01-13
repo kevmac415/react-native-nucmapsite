c3f534fe969341e1fa174174cffbd6d7ad3b163b
kevmac415 <kevmcfarland@yahoo.com>
2021-01-12 17:11:21 -0800

    Favorite component added

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -142,7 +142,7 @@ export const addPartners = partners => ({
     payload: partners
 });
 
-export const postFavorite = campsiteId => dispatch => {
+export const postFavorite = (campsiteId) => dispatch => {
     setTimeout(() => {
         dispatch(addFavorite(campsiteId));
     }, 2000);
@@ -152,3 +152,22 @@ export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
 });
+
+export const addComment = (comment) => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+})
+
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    const newComment = {
+        campsiteId,
+        rating,
+        author,
+        text,
+        date: new Date().toISOString()
+        
+    };
+    setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+};
